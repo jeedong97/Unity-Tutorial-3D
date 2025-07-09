6d121e4417a58190959779f469dc96c1cbcf444c
Dong <jeedong97@gmail.com>
2025-07-09 11:14:20 +0900

    Dev Script

---
+++ Assets/1.Data Structure/02. Scripts/NewMonoBehaviourScript.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    public int number = 100;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
