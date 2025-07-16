050570039df03119f7d434126a746bf340fcd127
Dong <jeedong97@gmail.com>
2025-07-16 17:58:27 +0900

    2dShooter

---
+++ Assets/3.Unity Book/02. Scripts/Background.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    public Material bgMaterial;
+    public float scrollSpeed = 0.2f;
+    private void Update()
+    {
+    Vector2 direction = Vector2.up;
+        bgMaterial.mainTextureOffset += direction * scrollSpeed * Time.deltaTime;
+    }
+}
